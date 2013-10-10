72599981a21c58110e570b7c55bcb00e4b0c6f6d
Michael Tingley <MichaelTingley@College.Harvard.edu>
2013-10-10 16:35:22 -0400

    Moved headers into folders, changed the MAKEFILE so that everything is called asst now. No more weirdness with the numbers. Let's aim to keep everything more organized.

---
+++ Assignment3/headers/rigtform.h
@@ -0,0 +1,75 @@
+#ifndef RIGTFORM_H
+#define RIGTFORM_H
+
+#include <iostream>
+#include <cassert>
+
+#include "matrix4.h"
+#include "quat.h"
+
+class RigTForm {
+  Cvec3 t_; // translation component
+  Quat r_;  // rotation component represented as a quaternion
+
+public:
+  RigTForm() : t_(0) {
+    assert(norm2(Quat(1,0,0,0) - r_) < CS175_EPS2);
+  }
+
+  RigTForm(const Cvec3& t, const Quat& r) {
+    //TODO
+  }
+
+  explicit RigTForm(const Cvec3& t) {
+    // TODO
+  }
+
+  explicit RigTForm(const Quat& r) {
+    // TODO
+  }
+
+  Cvec3 getTranslation() const {
+    return t_;
+  }
+
+  Quat getRotation() const {
+    return r_;
+  }
+
+  RigTForm& setTranslation(const Cvec3& t) {
+    t_ = t;
+    return *this;
+  }
+
+  RigTForm& setRotation(const Quat& r) {
+    r_ = r;
+    return *this;
+  }
+
+  Cvec4 operator * (const Cvec4& a) const {
+    // TODO
+  }
+
+  RigTForm operator * (const RigTForm& a) const {
+    // TODO
+  }
+};
+
+inline RigTForm inv(const RigTForm& tform) {
+  // TODO
+}
+
+inline RigTForm transFact(const RigTForm& tform) {
+  return RigTForm(tform.getTranslation());
+}
+
+inline RigTForm linFact(const RigTForm& tform) {
+  return RigTForm(tform.getRotation());
+}
+
+inline Matrix4 rigTFormToMatrix(const RigTForm& tform) {
+  // TODO
+  return m;
+}
+
+#endif
