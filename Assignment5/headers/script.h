aec3d513f1af5c0066780d1351f799c9b3250287
Michael Tingley <MichaelTingley@College.Harvard.edu>
2013-10-28 18:46:43 -0400

    NOW: with (moar) ANIMATIONS!

--- Assignment5/headers/script.h
+++ Assignment5/headers/script.h
@@ -212,8 +212,8 @@ public:
   void interpolate(float alpha, shared_ptr<SgRootNode> root) {
     Frame firstFrame = *iter;
     iter++;
-    Frame secondFrame = *iter;
     iter--;
+    Frame secondFrame = *iter;
     cout << "Trying to interpolate between " << firstFrame.DEBUG_STRING() << " and " << secondFrame.DEBUG_STRING() << "; alpha = " << alpha << endl;
     DEBUG();
 
